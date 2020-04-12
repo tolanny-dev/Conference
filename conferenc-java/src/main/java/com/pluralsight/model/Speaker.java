4e4b3e76e466b54ed3841775f33ed0bbd106565c
David Shittu <tol43454@gmail.com>
2020-04-11 19:11:55 -0500

    Initial commit of conference code with Java configuration

---
+++ conferenc-java/src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,33 @@
+package com.pluralsight.model;
+
+public class Speaker {
+
+
+    private String firstName;
+    private String lastName;
+    private Double seedNum;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastename) {
+        this.lastName = lastename;
+    }
+
+    public Double getSeedNum() {
+        return seedNum;
+    }
+
+    public void setSeedNum(Double seedNum) {
+        this.seedNum = seedNum;
+    }
+}
